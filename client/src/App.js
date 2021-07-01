2ca0ee0147631af578ccbc4c15e071d099ed3a3b
Nitish <nitishgautam1223@gmail.com>
2021-07-01 12:40:11 +0530

    change to ipfs-http-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import SolidityDriveContract from "./contracts/SolidityDrive.json";
 import getWeb3 from "./utils/getWeb3";
-import { StyledDropZone } from "react-drop-zone";
-import FileIcon, { defaultStyles } from "react-file-icon";
+import {StyledDropZone} from 'react-drop-zone';
+
+import {FileIcon,defaultStyles} from 'react-file-icon';
+
 import "react-drop-zone/dist/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Table } from "reactstrap";
+
+import {Table} from 'reactstrap';
 import fileReaderPullStream from 'pull-file-reader';
-import ipfs from './utils/ipfs';
-import Moment from "react-moment";
+import Moment from 'react-moment';
 import "./App.css";
+import { isUserEthereumAddressInBloom } from "web3-utils";
+const {create,urlSource}= require('ipfs-http-client');
+const ipfs= create({host:'localhost',port:5001,protocol:'http'
+});
+
 
 class App extends Component {
-  state = { solidityDrive: [], web3: null, accounts: null, contract: null };
+  state = { solidityDrive:[], web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -21,38 +28,37 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
+            console.log(accounts);
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SolidityDriveContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SolidityDriveContract.abi,
-        deployedNetwork && deployedNetwork.address
+        deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.getFiles);
-      web3.currentProvider.publicConfigStore.on('update', async () => {
+      web3.currentProvider.on('accountsChanged', async() =>{
         const changedAccounts = await web3.eth.getAccounts();
-        this.setState({accounts: changedAccounts});
+        
+        this.setState({accounts:changedAccounts});
         this.getFiles();
       })
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
+        `Failed to load web3, accounts, or contract. Check console for details.`,
       );
       console.error(error);
     }
   };
 
-  getFiles = async () => {
+  getFiles = async() => {
     try {
       const { accounts, contract } = this.state;
-      let filesLength = await contract.methods
-        .getLength()
-        .call({ from: accounts[0] });
+      let filesLength = await  contract.methods.getLength().call({ from: accounts[0] });
       let files = [];
       for (let i = 0; i < filesLength; i++) {
         let file = await contract.methods.getFile(i).call({ from: accounts[0] });
@@ -62,60 +68,57 @@ class App extends Component {
     } catch (error) {
      console.log(error);
     }
-  };
+  }
 
-  onDrop = async (file) => {
+  onDrop = async(file) =>{
     try {
-      const {contract, accounts} = this.state;
-      const stream = fileReaderPullStream(file);
-      const result = await ipfs.add(stream);
-      const timestamp = Math.round(+new Date() / 1000);
-      const type = file.name.substr(file.name.lastIndexOf(".")+1);
-      let uploaded = await contract.methods.add(result[0].hash, file.name, type, timestamp).send({from: accounts[0], gas: 300000})
+      const { accounts, contract } = this.state;
+      
+      const result=await ipfs.add(file);
+     
+      const generated_hash=result.cid.string;
+      console.log(generated_hash);
+      const timestamp= Math.round(+new Date()/1000); 
+      const type=file.name.substr(file.name.lastIndexOf(".")+1);
+      let uploaded=await contract.methods.add(generated_hash,file.name,type,timestamp).send({from:accounts[0],gas:300000});
       console.log(uploaded);
-      this.getFiles();
+    this.getFiles();
     } catch (error) {
       console.log(error);
+      
     }
-  };
+  }
+
 
   render() {
-    const {solidityDrive} = this.state;
+    const {solidityDrive}=this.state;
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <div className="container pt-3">
-          <StyledDropZone onDrop={this.onDrop} />
-          <Table>
-            <thead>
-              <tr>
-                <th width="7%" scope="row">
-                  Type
-                </th>
-                <th className="text-left">File Name</th>
-                <th className="text-right">Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {solidityDrive !== [] ? solidityDrive.map((item, key)=>(
-                <tr>
-                <th>
-                  <FileIcon
-                    size={30}
-                    extension={item[2]}
-                    {...defaultStyles[item[2]]}
-                  />
-                </th>
-                <th className="text-left"><a href={"https://ipfs.io/ipfs/"+item[0]}>{item[1]}</a></th>
-                <th className="text-right">
-                  <Moment format="YYYY/MM/DD" unix>{item[3]}</Moment>
-                </th>
-              </tr>
-              )) : null}
-            </tbody>
-          </Table>
+        <div className="container pt-5">
+           <StyledDropZone  onDrop={this.onDrop}/>
+           <Table>
+             <thead>
+               <tr>
+                 <th width="1%" scope="row">Type</th>
+                 <th className="px-3 " style={{textAlign:"left"}} >File Name</th>
+                 <th style={{textAlign:"right"}}>Date</th>
+               </tr>
+             </thead>
+             <tbody>
+               {solidityDrive !== [] ? solidityDrive.map((item,key) =>( 
+               <tr>
+               <th ><FileIcon  extension={item[2]} {...defaultStyles[item[2]]}/></th>
+                 <th className="px-3" style={{textAlign:"left"}} ><a href={"https://ipfs.io/ipfs/"+item[0]} target="_blank"> {item[1]}</a></th>
+                 <th style={{textAlign:"right"}}>
+                   <Moment format="YYYY/MM/DD" unix>{item[3]}</Moment>
+                 </th>
+               </tr>
+               )): null }
+             </tbody>
+           </Table>
         </div>
       </div>
     );
